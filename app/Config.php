8341d181cdae7980bb38a1849403991eb8d28831
martagithub8 <117286017+martagithub8@users.noreply.github.com>
2022-11-21 20:38:59 +0100

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -3,9 +3,9 @@
  class Config
  {
      static public $mvc_bd_hostname = "localhost";
-     static public $mvc_bd_nombre   = "alimentosalex";
-     static public $mvc_bd_usuario  = "myale7993";
-     static public $mvc_bd_clave    = "23mar2005";
+     static public $mvc_bd_nombre   = "alimentosa2022";
+     static public $mvc_bd_usuario  = "myjuancarladb";
+     static public $mvc_bd_clave    = "ndJlpv50";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
